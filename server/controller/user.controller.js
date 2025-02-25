091bdb01c698ec889d1070a76a01bb7f304a1d76
decent190 <samueloyelade6@gmail.com>
2025-02-25 16:09:36 +0100

    thirdcommit

--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,5 +1,5 @@
 const studentModel = require("../model/user.model")
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 require("dotenv").config()
 
